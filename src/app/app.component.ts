73103593cdff037722730345d4d624e35be08530
Megha Tare <meghatare123@gmail.com>
2024-02-13 15:19:53 +0530

    changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { OtpVerificationComponent } from './otp-verification/otp-verification.component';
+import { LoginComponent } from './login/login.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, SignInComponent, OtpVerificationComponent, LoginComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'AngularApp.ai';
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [HeaderComponent,RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
